078fa8f422dab6fc1fc684c1b8f7be7426645056
Jeremy Green <jeremy@octolabs.com>
2013-12-05 15:18:46 -0600

    Initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BackgroundSmackdown::Application.config.secret_key_base = '95b773ca77ef0b3436b30d480ae72bcbe4333df7fc1219b8ae99590041c0c89e82036ece6e05c98807edb75e19fc98824bd83692ceab7ef8d3024a1870f1e472'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BackgroundSmackdown::Application.config.session_store :cookie_store, key: '_background_smackdown_session'
